9a095ff53834b5982713b015204bbb6db6c5c764
avatoavenue <avenuefourdot@gmail.com>
2023-10-13 03:30:22 -0700

    transfer base finished

---
+++ principal/transfercash.js
@@ -0,0 +1,5 @@
+// Función para volver atrás
+document.getElementById("Menu").addEventListener("click", function () {
+    // Aquí puedes agregar la lógica para volver atrás, por ejemplo, redirigir a la página anterior
+    window.location.href = "menu.html";
+});
